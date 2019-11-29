5057d958a01b5ef20b2b9773113149587531ab55
Saif <saif.as7777@gmail.com>
2019-11-29 21:40:37 +0200

    update app to work with heroku

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const express = require( "express" );
 const app = express();
 const bodyParser = require( "body-parser" );
 const cors = require( "cors" );
+const path = require("path");
 const dependenciesRetrieverRoute = require( "./routes/dependencies-retriever.route" );
 
 
 app.use( cors() );
 app.use( bodyParser.json() );
+app.use(express.static(path.join(__dirname, "client/dist/dependency-graph-project")));
 app.use( "/api", dependenciesRetrieverRoute );
 
 
 const { PORT } = process.env;
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client/dist/dependency-graph-project/index.html"));
+});
 app.listen( PORT, () => {
 	console.log( `Server running on port ${PORT}` );
 } );
