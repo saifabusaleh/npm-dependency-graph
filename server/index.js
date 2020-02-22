e3bd0bdc06c01aa0daf5a8b20c307645b826606d
Saif <saif.as7777@gmail.com>
2020-02-22 16:14:10 +0200

    Refactor code structure

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use( "/api", dependenciesRetrieverRoute );
 const { PORT } = process.env;
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/*", (req, res) => {
-	res.sendFile(path.join(__dirname, "public/index.html"));
+	res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 app.listen( PORT, () => {
 	console.log( `Server running on port ${PORT}` );
