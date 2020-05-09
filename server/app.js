a0e3071ea9187056f89165349547f79808cca156
Saif <saif.as7777@gmail.com>
2020-05-09 11:31:58 +0300

    split index.js to app.js and server.js

--- server/app.js
+++ server/app.js
@@ -13,13 +13,11 @@ app.use( bodyParser.json() );
 app.use( "/api", dependenciesRetrieverRoute );
 
 
-const { PORT } = process.env;
 app.use(express.static(path.join(__dirname, "public")));
 app.get("/*", (req, res) => {
 	res.sendFile(path.join(__dirname, "public/index.html"));
 });
-app.listen( PORT, () => {
-	console.log( `Server running on port ${PORT}` );
-} );
+
+
 
 module.exports = app;
---
+++ server/server.js
@@ -0,0 +1,6 @@
+const app = require('./app.js')
+const { PORT } = process.env;
+
+app.listen( PORT, () => {
+	console.log( `Server running on port ${PORT}` );
+} );
